ede810c31f97772dab7a25834b6c64bd1e1e560c
Felipe Fernandes <felipe.fernandes.lima@usp.br>
2020-02-08 17:38:23 +0100

    Full adder completed

--- adder.py
+++ adder.py
@@ -21,11 +21,28 @@ s = SatVar('s')
 cout = SatVar('cout')
 
 # Internal variables (if needed)
+s0 = SatVar('s0')
+s1 = SatVar('s1')
+s2 = SatVar('s2')
+
+def gate_and(in1, in2, out) -> Cnf :
+    return (~in1|~in2|out) & (in1|~out) & (in2|~out)
+
+def gate_or(in1, in2, out) -> Cnf :
+    return (in1|in2|~out) & (~in1|out) & (~in2|out)
+
+def gate_xor(in1, in2, out) -> Cnf :
+    return (~in1|~in2|~out) & (in1|in2|~out) & (in1|~in2|out) & (~in1|in2|out)
 
 
 def mk_adder() -> Cnf:
+    add1 = gate_xor(a, b ,s0)
+    add2 = gate_xor(s0, cin, s)
+    add_cnf =  add1 & add2
 
-    # TODO: Construct CNF for full adder
-    # (The CNF returned below is pure nonsense...)
-    return (a | ~a) & (b | ~s) & ~cout & (b | cin)
+    carry1 = gate_and(a, b, s1)
+    carry2 = gate_and(s0, cin, s2)
+    carry3 = gate_or(s1, s2, cout)
+    carry_cnf = carry1 & carry2 & carry3
 
+    return add_cnf & carry_cnf
